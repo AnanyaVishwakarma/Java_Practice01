53b6cfa590dceb5d2e9e48505c1f225342e73447
AnanyaVishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-07-01 18:05:29 +0530

    Add files via upload

---
+++ javaAnagrams2.java
@@ -0,0 +1,63 @@
+/*Two strings,'a' and 'b', are called anagrams if they contain all the same characters in the same frequencies.
+For this challenge, the test is not case-sensitive. 
+For example, the anagrams of CAT are CAT, ACT, tac, TCA, aTC, and CtA.
+
+Function Description
+
+Complete the isAnagram function in the editor.
+
+isAnagram has the following parameters:
+
+string a: the first string
+string b: the second string
+Returns
+
+boolean: If 'a' and 'b' are case-insensitive anagrams, return true. Otherwise, return false. 
+Input Format
+
+The first line contains a string a.
+The second line contains a string b.
+
+Constraints
+1 <= length(a), length(b) <= 50.
+Strings a and b consist of English alphabetic characters.
+The comparison should NOT be case sensitive.*/
+
+import java.util.Scanner;
+
+public class javaAnagrams2 {
+
+    static boolean isAnagram(String a, String b) {
+
+        if (a.length() != b.length() ) {
+            return false ;
+        }
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+        int sum = 0;
+        for (char c  = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i < a.length(); i++) {
+                if (a.charAt(i)== c) {
+                    sum++;                
+            }
+            if (b.charAt(i)== c) {
+                sum--;
+            }
+        }
+            if (sum != 0) {
+                return false;
+            }
+        }
+          return true;      
+    }
+
+  public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
