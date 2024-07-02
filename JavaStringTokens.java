31efd7e0c65e60ba013267d3b9d18c97884d302f
AnanyaVishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-07-02 19:59:23 +0530

    Add files via upload

---
+++ JavaStringTokens.java
@@ -0,0 +1,54 @@
+/*Given a string,s, matching the regular expression [A-Za-z !,?._'@]+, split the string into tokens. 
+We define a token to be one or more consecutive English alphabetic letters. 
+Then, print the number of tokens, followed by each token on a new line.
+
+Note: You may find the 'String.split' method helpful in completing this challenge 
+Input Format:
+A single string, s.
+
+Constraints:
+1 >= length of s <= 4.10^9
+s is composed of any of the following: English alphabetic letters, blank spaces, 
+exclamation points (!), commas (,), question marks (?), periods (.), underscores (_), apostrophes ('), and at symbols (@).
+Output Format:
+
+On the first line, print an integer,n, denoting the number of tokens in string s (they do not need to be unique). 
+Next, print each of the n tokens on a new line in the same order as they appear in input string s.
+Sample Input
+
+He is a very very good boy, isn't he?
+Sample Output
+
+10
+He
+is
+a
+very
+very
+good
+boy
+isn
+t
+he
+*/
+
+
+import java.io.*;
+import java.util.*;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        String[] ary = s.split("[ !,?._'@]+");
+        int n = ary.length;
+        System.out.println(n);
+        for (int i = 0; i < n; i++){
+            System.out.println(ary[i]);
+        }
+        scan.close();
+    }
+}
+
