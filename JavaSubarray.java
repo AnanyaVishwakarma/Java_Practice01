27ce332da3fb8e48788622183b24f162495f6542
AnanyaVishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-07-24 21:51:52 +0530

    Add files via upload

---
+++ JavaSubarray.java
@@ -0,0 +1,47 @@
+/*We define the following:
+
+A subarray of an n-element array is an array composed from a contiguous block of the original array's elements. 
+For example, if array = [1,2,3], then the subarrays are [1], [2], [3], [1,2], [2,3], and [1,2,3]. 
+Something like [1,3] would not be a subarray as it's not a contiguous subsection of the original array.
+The sum of an array is the total sum of its elements.
+An array's sum is negative if the total sum of its elements is negative.
+An array's sum is positive if the total sum of its elements is positive.
+Given an array of  integers, find and print its number of negative subarrays on a new line.
+
+Input Format
+
+The first line contains a single integer,n , denoting the length of array A = [a0, a1,..., an-1].
+The second line contains n space-separated integers describing each respective element, ai, in array A.
+
+Constraints
+1 <= n <= 100
+-104 <= ai <= 10^4
+
+Output Format
+
+Print the number of subarrays of A having negative sums. */
+
+
+import java.io.*;
+import java.util.*;
+
+public class JavaSubarray {
+    
+    public static void subarray(int[] array, int start, int end){
+        if(start < 0 || end > array.length || start > end){
+            throw new IllegalArgumentException("Invalid start or end idices");
+        }
+        int[] subArray = Arrays.copyOfRange(array, start, end);
+        System.out.println(Arrays.toString(subArray));
+    }
+
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int n = scan.nextInt();
+       int[] array = new int[n];
+       for (int i = 0; i < array.length; i++) {
+           array[i]=scan.nextInt();
+       }
+       
+}
+}
