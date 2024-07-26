c8f83bd176f81ad47c669cb9d29ccf4d0a4072dd
AnanyaVishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-07-26 12:53:45 +0530

    Add files via upload

---
+++ MinIndexOfCommonString.java
@@ -0,0 +1,80 @@
+/*Given two arrays of strings list1 and list2, find the common strings with the least index sum.
+
+A common string is a string that appeared in both list1 and list2.
+
+A common string with the least index sum is a common string such that if it appeared at list1[i] and list2[j] 
+then i + j should be the minimum value among all the other common strings.
+
+Return all the common strings with the least index sum. Return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], 
+list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+Output: ["Shogun"]
+Explanation: The only common string is "Shogun".
+Example 2:
+
+Input: list1 = ["Shogun","Tapioca Express","Burger King","KFC"], 
+list2 = ["KFC","Shogun","Burger King"]
+Output: ["Shogun"]
+Explanation: The common string with the least index 
+sum is "Shogun" with index sum = (0 + 1) = 1.
+Example 3:
+
+Input: list1 = ["happy","sad","good"], list2 = ["sad","happy","good"]
+Output: ["sad","happy"]
+Explanation: There are three common strings:
+"happy" with index sum = (0 + 1) = 1.
+"sad" with index sum = (1 + 0) = 1.
+"good" with index sum = (2 + 2) = 4.
+The strings with the least index sum are "sad" and "happy".
+ 
+
+Constraints:
+
+1 <= list1.length, list2.length <= 1000
+1 <= list1[i].length, list2[i].length <= 30
+list1[i] and list2[i] consist of spaces ' ' and English letters.
+All the strings of list1 are unique.
+All the strings of list2 are unique.
+There is at least a common string between list1 and list2 */
+
+import java.util.*;
+
+class MinIndexOfCommonString {
+    public static String[] findRestaurant(String[] list1, String[] list2) {
+        int min = Integer.MAX_VALUE;
+        String[] res = new String[0];
+        for (int i = 0; i < list1.length; i++) {
+            for (int j = 0; j < list2.length; j++) {
+                if (list1[i].equals(list2[j])) {
+                    int sum = i + j;
+                    if (sum < min) {
+                        min = sum;
+                        res = new String[1];
+                        res[0] = list1[i];
+                        } else if (sum == min) {
+                        res = Arrays.copyOf(res, res.length + 1);
+                        res[res.length - 1] = list1[i];
+                            }
+                    }
+                        }
+            }
+        }
+        return ;
+        
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        String[] list01 = scan.nextLine().split("");
+        String[] list02 = scan.nextLine().split("");
+        String[] result = findRestaurant(list01, list02);
+        System.out.println(Arrays.toString(result));
+
+    }
+}
