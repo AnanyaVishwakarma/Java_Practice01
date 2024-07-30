8f104993a7e950c66e7e4aa28471300abce20cf8
AnanyaVishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-07-30 19:05:36 +0530

    Update java1Darray02.java

--- java1Darray02.java
+++ java1Darray02.java
@@ -57,18 +57,16 @@ import java.util.*;
 public class java1Darray02 {
 
     public static boolean canWin(int leap, int[] game) {
-        // Return true if you can win the game; otherwise, return false.
-        int n = game.length;
-        boolean[] dp = new boolean[n];
-        dp[0] = false;
-        for (int i = 1; i < n; i++) {
-            if (i - leap >= 0 && dp[i - leap]) {
-                dp[i] = true;
-                }
-                }
-                return dp[n - 1];
+        return helper(leap, game, 0);
     }
+    private static boolean helper(int leap, int[] game, int i){
+        if (i >= game.length) return true;
+        if (i < 0 || game[i] == 1) return false;
+        game[i] = 1;
 
+        return helper(leap, game, i + 1)|| helper(leap, game, i - 1) || helper(leap, game, i + leap);
+
+    }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int q = scan.nextInt();
