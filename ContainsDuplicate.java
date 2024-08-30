29f577e050cc19fd5c10efdaefbeac9ede3672bc
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-08-30 22:24:02 +0530

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -17,12 +17,11 @@ Output: true */
 
 public class ContainsDuplicate {
     public boolean ContainsDuplicate(int[] nums){
-        for (int i = 0; i < nums.length; i++) {
-             for (int j = 0; j < nums.length; j++) {
-                if(i != j && nums[i] == nums[j]){
-                    return true;
-                }
-             }
+        HashSet<Integer> set = new HashSet<>();
+        for (Integer num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
         }
         return false;
     }
