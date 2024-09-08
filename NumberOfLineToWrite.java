a68b2936a807915bd159e6386e40867b7a293679
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-09-08 23:14:58 +0530

    Add files via upload

---
+++ NumberOfLineToWrite.java
@@ -0,0 +1,29 @@
+/*You are given a string s of lowercase English letters and an array widths denoting how many pixels 
+wide each lowercase English letter is. Specifically, widths[0] is the width of 'a', widths[1] is the width of 'b', and so on.
+
+You are trying to write s across several lines, where each line is no longer than 100 pixels. Starting at 
+the beginning of s, write as many letters on the first line such that the total width does not exceed 100 pixels. 
+Then, from where you stopped in s, continue writing as many letters as you can on the second line. Continue this process 
+until you have written all of s.
+
+Return an array result of length 2 where:
+
+result[0] is the total number of lines.
+result[1] is the width of the last line in pixels */
+
+public class NumberOfLineToWrite {
+    public int[] numberOfLines(int[] widths, String s) {
+        int line = 1;
+        int totalWidth = 0;
+        for (char c : s.toCharArray()) {
+            int char_width = widths[c - 'a'];
+            if (totalWidth + char_width > 100){
+                line++;
+                totalWidth = char_width;
+            } else{
+                totalWidth += char_width;
+            }
+        }
+        return new int[]{line, totalWidth};
+    }    
+}
