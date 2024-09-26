c80830eec8eb1edb75861987eae19b9ebb1f2857
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-09-26 22:12:25 +0530

    GroupAnagrams.java

---
+++ GroupAnagrams.java
@@ -0,0 +1,48 @@
+/*Given an array of strings strs, group the 
+anagrams
+ together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]] */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        for(String str : strs) {
+            char[] charArray = str.toCharArray();
+            int[] count = new int[26];
+            for (char c  : charArray){
+                count[c - 'a']++;
+            }
+            boolean found = false;
+
+        }
+        }
+    }
+
