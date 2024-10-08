ba99bfa79e2467aac1486ff1a5b8cc0d7841f66d
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-10-09 00:07:29 +0530

    shortestDistanceToACharacter.java

--- shortestDistanceToACharacter.java
+++ shortestDistanceToACharacter.java
@@ -27,9 +27,22 @@ public class shortestDistanceToACharacter {
         int[] answer = new int[n];
         int prev = Integer.MIN_VALUE /2 ;
 
-        for (int i =0; i < n; i++){
-            
+        for (int i = 0; i < n; i++){
+            if(s.charAt(i) == c){
+                prev = i;
+            }
+            answer[i] = i - prev;
         }
+
+        prev = Integer.MAX_VALUE /2;
+        for (int i = n -1; i >= 0; i--){
+            if (s.charAt(i) == c){
+                prev = i;
+            }
+            answer[i] = Math.min(answer[i], prev - i);
+        }
+        return answer;
+    }  
 
     }    
 }
