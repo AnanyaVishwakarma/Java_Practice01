0d2912c95b09fa8b34055a283820534ee8ac0e1d
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-10-09 23:33:25 +0530

    Add files via upload

---
+++ findCommonCharacters.java
@@ -0,0 +1,29 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*Given a string array words, return an array of all characters that show up in all strings within the words 
+(including duplicates). You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: words = ["bella","label","roller"]
+Output: ["e","l","l"]
+Example 2:
+
+Input: words = ["cool","lock","cook"]
+Output: ["c","o"] */
+
+public class findCommonCharacters {
+    public List<String> commonChars(String[] words) {
+        
+
+        Map<Character, String> freqMap = new HashMap<>();
+        for (int i =0; i < words.length; i++){
+
+        }
+
+    }    
+}
