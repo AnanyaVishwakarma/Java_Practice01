70551732e7fe26a46261d9407dc9e05ff7cfd2c7
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-10-11 00:08:22 +0530

    findCommonCharacters.java

--- findCommonCharacters.java
+++ findCommonCharacters.java
@@ -18,12 +18,30 @@ Output: ["c","o"] */
 
 public class findCommonCharacters {
     public List<String> commonChars(String[] words) {
-        
+       List<String> result = new ArrayList<>();
 
-        Map<Character, String> freqMap = new HashMap<>();
+        Map<Character, Integer> freqMap = new HashMap<>();
+        for (char c  : words[0].toCharArray()){
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
         for (int i =0; i < words.length; i++){
+            Map<Character, Integer> wordMap = new HashMap<>();
+            for(char c : words[i].toCharArray()){
+                wordMap.put(c, wordMap.getOrDefault(c, 0) + 1);
+            }
+
+            for ( char c: freqMap.keySet()){
+                if (wordMap.containsKey(c)){
+                    freqMap.put(c, Math.min(freqMap.get(c), wordMap.get(c)));
+                } else{
+                    freqMap.put(c, 0);
+                }
+            }
+            
 
         }
+
 
     }    
 }
