696ff990b1a6fbe22025c0d6975aeb149306e928
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-10-18 22:55:24 +0530

    Update rotateArray.java

--- rotateArray.java
+++ rotateArray.java
@@ -1,6 +1,16 @@
 public class rotateArray {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
+
+        int[] temp = new int[k];
+        
+        for(int i = 0; i < n; i++){
+            temp[i] = nums[n - k + i];
+        }
+
+        for ( int i = n-1; i >= k; i--){
+            nums[i] = nums [i -k];
+        }        
         
     }
 }
