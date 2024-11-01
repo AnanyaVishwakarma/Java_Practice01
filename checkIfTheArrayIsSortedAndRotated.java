b1caf07edf6aef1c31d391fd3061be12d6131100
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-11-01 21:57:48 +0530

    Update checkIfTheArrayIsSortedAndRotated.java

--- checkIfTheArrayIsSortedAndRotated.java
+++ checkIfTheArrayIsSortedAndRotated.java
@@ -29,24 +29,16 @@ You can rotate the array by x = 0 positions (i.e. no rotation) to make nums. */
 public class checkIfTheArrayIsSortedAndRotated {
     public static boolean check(int[] nums) {
         int n = nums.length;
-        for(int i = 1; i < n; i++){
-            if(nums[i] < nums[i-1]){
-                return false;
+     int dropCount = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > nums[(i + 1) % n]){
+                dropCount++;
             }
+         if (droupCount > 1){
+          return false;
         }
+        }   
         return true;
     }    
-    public  static boolean canBeSorted(int arr[]){
-        int n = arr.length;
-        for (int i = 0; i < n; i++){
-            int rotatedArray[] = new int[n];
-            for (int j = 0; j < n; j++){
-                rotatedArray[j] = arr[(i + j) % n];
-            }
-            if (check(rotatedArray)){
-                return true;
-            }
-        }
-        return false;
-    } 
+
 }
