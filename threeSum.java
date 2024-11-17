04e9e3afb6b0ff9e34cc1b257b95da7a01af7493
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-11-17 17:43:22 +0530

    Update threeSum.java

--- threeSum.java
+++ threeSum.java
@@ -30,6 +30,16 @@ import java.util.List;
 
 public class threeSum {
     public List<List<Integer>> threeSum(int[] nums) {
+       Arrays.sort(nums);
+
+        for(int i = 0;i < nums.length-2;i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+        }
+        
         
     }    
 }
