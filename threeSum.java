df57bfb44edb9fd98ca25495503f939217d2d2fe
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-11-18 19:32:12 +0530

    Update threeSum.java

--- threeSum.java
+++ threeSum.java
@@ -38,6 +38,12 @@ public class threeSum {
             }
             int left = i + 1;
             int right = nums.length - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                }         
         }
         
         
