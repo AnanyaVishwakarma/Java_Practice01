177608adaacb3ee8d8d5a135f372160dbdd8b861
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-11-19 21:31:06 +0530

    Update threeSum.java

--- threeSum.java
+++ threeSum.java
@@ -43,9 +43,19 @@ public class threeSum {
 
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                }         
+
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }else if(sum < 0){
+                    left++;
+                } else{
+                    right--;
+                }
+            }
         }
-        
-        
-    }    
+        return result;
+    }  
 }
