645b6dff71e2ed2a8d7ba29f243c73e24d6381d4
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-11-28 09:45:44 +0530

    Add files via upload

---
+++ smallestAmongTheArray.java
@@ -0,0 +1,10 @@
+public class smallestAmongTheArray {
+    public static int smallestInteger (int[] nums){
+        int n = nums.length;
+        for (int i = 0; i < n; i++){
+            if (nums[i] < nums[n]) {
+                
+            }
+        }
+    }
+}
