b2fac63901b45bdd81262ad839529dc09518546a
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-11-28 21:52:05 +0530

    Update smallestAmongTheArray.java

--- smallestAmongTheArray.java
+++ smallestAmongTheArray.java
@@ -1,10 +1,11 @@
 public class smallestAmongTheArray {
     public static int smallestInteger (int[] nums){
-        int n = nums.length;
-        for (int i = 0; i < n; i++){
-            if (nums[i] < nums[n]) {
-                
-            }
+        int min = nums[0];
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] < min){
+                min = nums[i];
+            }            
         }
+        return min;
     }
 }
