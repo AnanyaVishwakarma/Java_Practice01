220611b8811392eea39fbbe154c76fd09f359709
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-11-30 09:39:13 +0530

    Add files via upload

---
+++ averageOfNumbersInArray.java
@@ -0,0 +1,24 @@
+
+import javax.crypto.NullCipher;
+
+/*The goal is to calculate the average of the numbers in an array.
+
+Definition of Average:
+
+Average=   Number of Elements/Sum of All Elements
+​
+For example, if the array is {10, 20, 30, 40, 50}, the sum is:
+
+10+20+30+40+50=150
+The number of elements is 5, so:
+
+Average=150/5 = 30 */
+public class averageOfNumbersInArray {
+    public int avgOfElements (int[] nums){
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++ ){
+            
+        }
+    }
+}
