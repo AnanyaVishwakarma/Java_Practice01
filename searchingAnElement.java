b4152a3642a31fabd7b9b3acb20c6bb2fa71f24a
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-12-03 10:52:18 +0530

    Add files via upload

---
+++ searchingAnElement.java
@@ -0,0 +1,18 @@
+/*You are asked to search for a specific element in an array. 
+An array is a collection of elements (e.g., integers, strings) stored at contiguous memory locations.
+
+Key Objective:
+You are given:
+An array of elements.
+An element to search for (also known as the target).
+The task is to check if the element exists in the array. If it does:
+Return or print its index (position in the array).
+If it doesn’t exist, return or print a message like "Element not found." */
+
+public class searchingAnElement {
+    public int searchElem(int[] nums){
+        int x;
+        int n = nums.length;
+
+    }   
+}
