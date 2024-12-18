86e8342c0fa1efa856e52f5ecbfdecb592619c22
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-12-18 22:10:14 +0530

    Update maximumSubarraySumWithLengthDivisibleByK.java

--- maximumSubarraySumWithLengthDivisibleByK.java
+++ maximumSubarraySumWithLengthDivisibleByK.java
@@ -59,8 +59,4 @@ public class maximumSubarraySumWithLengthDivisibleByK {
         }  
         return maxSum;
     }
-    public static void main(String[] args) {
-        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println("Maximum Subarray Sum: " + maxSubarraySum(nums));
-    }    
 }
