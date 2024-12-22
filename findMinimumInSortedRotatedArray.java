eae034657bc8a2354715d344528f67bf0773bf99
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2024-12-22 19:45:28 +0530

    Update findMinimumInSortedRotatedArray.java

--- findMinimumInSortedRotatedArray.java
+++ findMinimumInSortedRotatedArray.java
@@ -34,7 +34,9 @@ public class findMinimumInSortedRotatedArray {
     public int findMin(int[] nums) {
      int low = 0; int high = nums.length;
      int mid = low + high/2;
-        
+        if(nums[mid]>nums[high]){
+         low = mid +1;
+        }
 
     }
 }
