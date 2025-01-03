1d5595fb9bac3f5e59719f8910090b5b48f15055
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-01-03 12:20:12 +0530

    Update searchInARotatedSortedArray.java

--- searchInARotatedSortedArray.java
+++ searchInARotatedSortedArray.java
@@ -37,9 +37,11 @@ public class searchInARotatedSortedArray {
             high = mid-1;
          }
             low = mid + 1;
-         }
-
+         } else {
+         if (target > nums[mid] && target <= nums[high]){
+          low = mid + 1;
         }
+         high = mid - 1;
      }
      }
      return -1;
