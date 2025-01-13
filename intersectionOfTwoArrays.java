8fdf99acb1fec4682ad927c91480c52a79edaec3
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-01-13 19:34:48 +0530

    Update intersectionOfTwoArrays.java

--- intersectionOfTwoArrays.java
+++ intersectionOfTwoArrays.java
@@ -24,5 +24,8 @@ public class intersectionOfTwoArrays {
          int left = 0; int right = arr.length - 1;
          while (left <= right){
          int mid  = left + (right - left) / 2 ;
+         if(arr[mid] == target){
+         return true;
+         } 
     }
  }
