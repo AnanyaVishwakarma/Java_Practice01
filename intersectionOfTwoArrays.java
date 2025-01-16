fc4157e1fc38118d6c1f159ff477fabdab108017
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-01-16 19:50:03 +0530

    Update intersectionOfTwoArrays.java

--- intersectionOfTwoArrays.java
+++ intersectionOfTwoArrays.java
@@ -1,9 +1,6 @@
 /*Given two integer arrays nums1 and nums2, return an array of their 
 intersection
 . Each element in the result must be unique and you may return the result in any order.
-
- 
-
 Example 1:
 
 Input: nums1 = [1,2,2,1], nums2 = [2,2]
@@ -31,7 +28,9 @@ public class intersectionOfTwoArrays {
      }
         int[] result = new int[result(resultset.size()];
      int index = 0;
-     for (int num : resultset){}
+     for (int num : resultset){
+      result[index++] = num;
+     }
     }    
 
     public static boolean binarySearch ( int[] arr, int target){
