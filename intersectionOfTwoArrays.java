bc75ec118a330850ca9e07c4f4747a1917942215
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-01-21 14:37:33 +0530

    Update intersectionOfTwoArrays.java

--- intersectionOfTwoArrays.java
+++ intersectionOfTwoArrays.java
@@ -53,5 +53,8 @@ public class intersectionOfTwoArrays {
         int[] nums1 = {1, 2, 2, 1};
         int[] nums2 = {2, 2};
         System.out.println(Arrays.toString(intersection(nums1, nums2)));
+
+        nums1 = new int[]{4, 9, 5};
+        nums2 = new int[]{9, 4, 9, 8, 4};
     }
  }
