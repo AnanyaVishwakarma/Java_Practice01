42e95ede24b4d09fdbc06c3eebdb7a3bdad1d829
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-01-25 20:03:31 +0530

    Update searchInRotatedSortedArrayII.java

--- searchInRotatedSortedArrayII.java
+++ searchInRotatedSortedArrayII.java
@@ -17,5 +17,8 @@ public class searchInRotatedSortedArrayII {
     public boolean search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
+        while ( left < right){
+        int mid = left + (right - left) / 2;
+        }
     }
 }
