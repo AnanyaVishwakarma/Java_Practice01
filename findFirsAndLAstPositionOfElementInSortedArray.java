6208067a96ba59f1894799ed906c46b310c90f68
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-01-31 11:29:54 +0530

    Update findFirsAndLAstPositionOfElementInSortedArray.java

--- findFirsAndLAstPositionOfElementInSortedArray.java
+++ findFirsAndLAstPositionOfElementInSortedArray.java
@@ -16,6 +16,9 @@ public class findFirsAndLAstPositionOfElementInSortedArray {
     public int[] searchRange(int[] nums, int target) {
     }
     private static int findFirst(int[] nums, int target){
-        int start = 0, end = nums.length - 1, first = -1;
+        int start = 0; int end = nums.length - 1; int first = -1;
+        while (start <= end){
+        int mid =  start + (end - start) / 2;
+        }
     }
 }
