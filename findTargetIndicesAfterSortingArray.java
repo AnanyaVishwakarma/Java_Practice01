ed156b71fd87363e5209a76ea348bdaaa3ccfa50
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-02-04 21:18:23 +0530

    Update findTargetIndicesAfterSortingArray.java

--- findTargetIndicesAfterSortingArray.java
+++ findTargetIndicesAfterSortingArray.java
@@ -29,7 +29,7 @@ public class findTargetIndicesAfterSortingArray {
     private int firstIndex (int[] nums, int target){
      int start =0; int end = nums.length-1;
         while(start <= end){
-        
+        int mid = start + (end - start)/2;
         }
     }
     
