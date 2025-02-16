67c470584477cb77f6c12c9a87dbdaf6c8c14a52
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-02-16 21:09:14 +0530

    Update fairCandySwap.java

--- fairCandySwap.java
+++ fairCandySwap.java
@@ -39,8 +39,13 @@ public class fairCandySwap {
         int  left =0; int right = nums.length-1; 
         while (left <= right) {
         int mid = left + (right + left)/2;
-
-        
+            if(target == nums[mid]){
+                return  true;
+            } else if(target < nums[mid]){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-    }
+        return false;
 }
