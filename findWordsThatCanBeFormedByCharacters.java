2f481ad1820f3810926b371b062ff95afa0a3463
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-02-25 19:05:07 +0530

    Update findWordsThatCanBeFormedByCharacters.java

--- findWordsThatCanBeFormedByCharacters.java
+++ findWordsThatCanBeFormedByCharacters.java
@@ -12,5 +12,8 @@ Output: 10
 Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10. */
 
 public class findWordsThatCanBeFormedByCharacters {
+    public int countCharacters(String[] words, String chars) {
+
+    }    
     
 }
