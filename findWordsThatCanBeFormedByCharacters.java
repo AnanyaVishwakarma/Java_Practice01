87015d9f3082578a01f146864bb96a29378fc622
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-03-01 21:04:03 +0530

    Update findWordsThatCanBeFormedByCharacters.java

--- findWordsThatCanBeFormedByCharacters.java
+++ findWordsThatCanBeFormedByCharacters.java
@@ -31,7 +31,7 @@ public class findWordsThatCanBeFormedByCharacters {
         }
      }
         if(canBeFormed){
-            
+            for(char ch: word.toCharArray()){}
         }
         }
 
