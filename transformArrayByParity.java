32904a0a32f1af885f809e7dc0d23478646868df
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-03-27 13:46:41 +0530

    Update transformArrayByParity.java

--- transformArrayByParity.java
+++ transformArrayByParity.java
@@ -26,6 +26,8 @@ public class transformArrayByParity {
             if(nums[i] % 2 == 0){
                  nums[i] = 0;
             } else{
-               
+                nums[i] = 1;
+            }
+        }               
         }    
 }
