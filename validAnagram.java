4f9f6c895504ecb4611fa1176fc4f9e413d34dac
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-04-07 17:16:25 +0530

    Update validAnagram.java

--- validAnagram.java
+++ validAnagram.java
@@ -10,6 +10,7 @@ public class validAnagram {
         if(s.length() == t.length()){
             char[] sArray = s.toCharArray();
             char[] tArray = t.toCharArray();
+            String sortedString = sortingArray(s);
         }        
     }
         private String sortingArray(String s){
