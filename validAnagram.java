97a82d37c6612eab1c1867e089749445fbf632eb
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-04-07 17:15:15 +0530

    Update validAnagram.java

--- validAnagram.java
+++ validAnagram.java
@@ -24,8 +24,6 @@ public class validAnagram {
                 }  
                 System.out.println();
             }
-        
-
-        return s;
+        return Arrays.toString(sArray);
     } 
 }
