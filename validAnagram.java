3be098e78c6050daa2a5b8c87472c137b87ca80e
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-04-08 21:12:21 +0530

    Update validAnagram.java

--- validAnagram.java
+++ validAnagram.java
@@ -13,6 +13,7 @@ public class validAnagram {
         int[] count = new int[26];
         for(int  i = 0; i < s.length(); i++){
             count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
         
     }
