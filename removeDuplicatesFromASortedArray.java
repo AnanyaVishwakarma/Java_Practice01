40a1afb95abe4e7360b96cc3552b715adeeaeb2c
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-04-15 22:29:38 +0530

    Update removeDuplicatesFromASortedArray.java

--- removeDuplicatesFromASortedArray.java
+++ removeDuplicatesFromASortedArray.java
@@ -30,7 +30,9 @@ public class removeDuplicatesFromASortedArray {
     public int removeDuplicates(int[] nums) {
         if(nums.length <= 2) return nums.length;
         int i = 2;
-        for(int j = 2;        
+        for(int j = 2;j < nums.length; j++){
+            
+        }   
         return 0;
     }
 }
