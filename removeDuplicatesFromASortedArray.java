b4e46dd3c6996483fb0ede2bc3acc277e16352f5
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-04-15 22:28:23 +0530

    Update removeDuplicatesFromASortedArray.java

--- removeDuplicatesFromASortedArray.java
+++ removeDuplicatesFromASortedArray.java
@@ -29,6 +29,7 @@ It does not matter what you leave beyond the returned k (hence they are undersco
 public class removeDuplicatesFromASortedArray {
     public int removeDuplicates(int[] nums) {
         if(nums.length <= 2) return nums.length;
+        int i = 2;
         for(int j =         
         return 0;
     }
