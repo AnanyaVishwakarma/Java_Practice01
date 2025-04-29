0972bd53f28dad61807f387fb82741468673c0e7
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-04-29 23:26:31 +0530

    Update primeNumber.java

--- primeNumber.java
+++ primeNumber.java
@@ -1,7 +1,7 @@
 public class primeNumber {
 public boolean isPrime(int num){
   if(num <= 1) return false; 
-  for(int i = 2; i < num/2; i++){
+  for(int i = 2; i <= num/2; i++){
     
   }
     return true;
