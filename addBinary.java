7e861e7915d7358afd0f1b775de934b400f27f0b
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-15 17:37:12 +0530

    Update addBinary.java

--- addBinary.java
+++ addBinary.java
@@ -10,6 +10,7 @@ public class addBinary {
     public String addBinary(String a, String b) {        
         int num1 = Integer.parseInt(a, 2);
         int num2 = Integer.parseInt(b, 2);
-        
+
+        int sum = num1 + num2;
     }    
 }
