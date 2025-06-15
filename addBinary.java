3410722d0857d29f3e4d138b65ef5a49e3533bdb
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-15 17:31:42 +0530

    Update addBinary.java

--- addBinary.java
+++ addBinary.java
@@ -10,6 +10,7 @@ public class addBinary {
     public String addBinary(String a, String b) {
      StringBuilder sb = new StringBuilder();
         int i = a.length() -1;
+        int j = b.length() -1;
            
     }    
 }
