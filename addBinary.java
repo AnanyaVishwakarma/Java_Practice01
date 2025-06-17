c3c32932555a56d6d1291d0e76f91761596ec978
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-17 18:47:46 +0530

    Update addBinary.java

--- addBinary.java
+++ addBinary.java
@@ -10,8 +10,8 @@ public class addBinary {
     public String addBinary(String a, String b) {        
        BigInteger num1 = new BigInteger(a, 2);
         BigInteger num2 = new BigInteger(b, 2);
-
-        int sum = num1 + num2;
+        BigInteger sum = num1.add(num2);
+        
 
         return Integer.toBinaryString(sum);
     }    
