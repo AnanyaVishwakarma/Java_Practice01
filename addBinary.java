4ad9a4fad18c65e3484dab7d2f90ea2f694572b0
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-17 18:46:33 +0530

    Update addBinary.java

--- addBinary.java
+++ addBinary.java
@@ -8,7 +8,7 @@ Output: "10101"
  */
 public class addBinary {
     public String addBinary(String a, String b) {        
-        int num1 = Integer.parseInt(a, 2);
+       BigInteger num1 = new BigInteger(a, 2);
         int num2 = Integer.parseInt(b, 2);
 
         int sum = num1 + num2;
