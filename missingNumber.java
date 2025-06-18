53bced798eb3da141af5a04d8334b406125edbb5
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-18 21:32:25 +0530

    Update missingNumber.java

--- missingNumber.java
+++ missingNumber.java
@@ -1,3 +1,4 @@
+
 /*Given an array nums containing n distinct numbers in the range [0, n], 
 return the only number in the range that is missing from the array.
 Example 1:
@@ -22,6 +23,8 @@ public class missingNumber {
     public int missingNumber(int[] nums) {
         int n = nums.length;
         int num = -1;
+
+        Arrays.sort(nums);
 
     }    
 }
