67c9dfce23fd1b386443cd50ad05e16610cfdec0
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-18 21:33:49 +0530

    Update missingNumber.java

--- missingNumber.java
+++ missingNumber.java
@@ -28,7 +28,7 @@ public class missingNumber {
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
                 if(nums[i] != j){
-
+                  num = j;
                 }
             }
         }
