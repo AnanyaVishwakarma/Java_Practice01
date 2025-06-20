3bfeff9a0aef1df218cb0c8f82354ec110015fef
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-20 23:16:15 +0530

    Update maximumSubarraySumWithLengthDivisibleByK.java

--- maximumSubarraySumWithLengthDivisibleByK.java
+++ maximumSubarraySumWithLengthDivisibleByK.java
@@ -20,8 +20,7 @@ The maximum sum subarray is [1, 2, -3, 4] which has length equal to 4 which is d
 public class maximumSubarraySumWithLengthDivisibleByK {
     public long maxSubarraySum(int[] nums, int k) {
             int n = nums.length;
-
-          
+            for(int i = 0; i < n; i++){
         
     }
         public static void main(String[] args) {
