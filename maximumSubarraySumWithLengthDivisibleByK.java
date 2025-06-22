0daa851619f136e5debf3e5d76f5d1816f064dcd
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-22 23:21:02 +0530

    Update maximumSubarraySumWithLengthDivisibleByK.java

--- maximumSubarraySumWithLengthDivisibleByK.java
+++ maximumSubarraySumWithLengthDivisibleByK.java
@@ -26,7 +26,7 @@ public class maximumSubarraySumWithLengthDivisibleByK {
                 for (int j = i; j < n; j++) {
                 int len = j-i+1;
                     if(len % k == 0){
-                      int sum =0;  
+                      long sum =0;  
                         for(int m = i; m < j; m++){
                              sum += nums[m];
                             maxSum = Math.max(maxSum, sum);
