7749c31e224dcf5b75dff86b3db7fae34993a55b
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-06-29 17:38:22 +0530

    Update maximumSubarraySumWithLengthDivisibleByK.java

--- maximumSubarraySumWithLengthDivisibleByK.java
+++ maximumSubarraySumWithLengthDivisibleByK.java
@@ -32,7 +32,7 @@ public class maximumSubarraySumWithLengthDivisibleByK {
                int len = j - i +1;
            if(len % k == 0){
                long sum = prefixSum[j+1] - prefixSum[i];
-               
+               maxSum = Math.max(maxSum, sum);  
               }
             }
         }
