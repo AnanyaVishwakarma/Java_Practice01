0cc5eb95f798be0a18dc3e04015159e36b51b3d3
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-07-06 21:41:37 +0530

    Update defuseTheBomb.java

--- defuseTheBomb.java
+++ defuseTheBomb.java
@@ -24,15 +24,16 @@ public class defuseTheBomb {
      int[] result = new int[n];
 
      if(k == 0) return result;
+        
         for(int i = 0; i <n; i++){
             int sum =0;
             if(k > 0){
                 for(int j = 1; j <=k; j++){
-                    sum += code[(i + j)%n];
-                }else{
-                 for(){}
+                    sum += code[(i + j)%n];                
                 }
-            }
+            }else{
+                 for(){}
+             }
         }
        
         return result;
