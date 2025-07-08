3e8819bcea2ed4eee27bb5d37752c735e08a469f
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-07-08 22:51:31 +0530

    Update concatenationOfAnArray.java

--- concatenationOfAnArray.java
+++ concatenationOfAnArray.java
@@ -25,6 +25,8 @@ public class concatenationOfAnArray {
     public int[] getConcatenation(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n*2];
+        
+        for(int i = 0; i < n; i++){}
 
     }    
 }
