cd7c2584ef0f9af57ef2143c2b8d6a57ab5930de
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-07-12 22:05:39 +0530

    Update MinimumNumberofOperationstoMoveAllBallstoEachBox .java

--- MinimumNumberofOperationstoMoveAllBallstoEachBox .java
+++ MinimumNumberofOperationstoMoveAllBallstoEachBox .java
@@ -28,7 +28,6 @@ public class MinimumNumberofOperationstoMoveAllBallstoEachBox  {
             for (int j = 0; j < n; j++) {
                 if (arr[j] == '1') {
                     cost += Math.abs(i - j);                    
-                    ans[i] = cost;
                 }
             }            
         }
