1792cf2f65966e592fd44d2067cf0eeab5502ccc
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-07-27 22:40:08 +0530

    Update sortingTheSentence.java

--- sortingTheSentence.java
+++ sortingTheSentence.java
@@ -24,6 +24,8 @@ public class sortingTheSentence {
 
         for(String word: words){
             int pos = word.charAt(word.length() - 1) - '0';
+
+            result[pos - 1] = word.substring(0, word.length() - 1);
         }
         
         
