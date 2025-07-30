dd3808780faff15171ccb83bce2bcbce4a54933e
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-07-30 21:35:43 +0530

    Update minimumNumberofMovestoSeatEveryone.java

--- minimumNumberofMovestoSeatEveryone.java
+++ minimumNumberofMovestoSeatEveryone.java
@@ -1,16 +1,10 @@
-/*There are n availabe seats and n students standing in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
-
+/*There are n availabe seats and n students standing in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. 
+You are also given the array students of length n, where students[j] is the position of the jth student.
 You may perform the following move any number of times:
-
 Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
 Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
-
 Note that there may be multiple seats or students in the same position at the beginning.
-
- 
-
 Example 1:
-
 Input: seats = [3,1,5], students = [2,7,4]
 Output: 4
 Explanation: The students are moved as follows:
@@ -19,7 +13,6 @@ Explanation: The students are moved as follows:
 - The third student is moved from position 4 to position 3 using 1 move.
 In total, 1 + 2 + 1 = 4 moves were used.
 Example 2:
-
 Input: seats = [4,1,5,9], students = [1,3,2,6]
 Output: 7
 Explanation: The students are moved as follows:
@@ -29,7 +22,6 @@ Explanation: The students are moved as follows:
 - The fourth student is moved from position 6 to position 9 using 3 moves.
 In total, 0 + 1 + 3 + 3 = 7 moves were used.
 Example 3:
-
 Input: seats = [2,2,6,6], students = [1,3,2,6]
 Output: 4
 Explanation: Note that there are two seats at position 2 and two seats at position 6.
@@ -39,10 +31,7 @@ The students are moved as follows:
 - The third student is not moved.
 - The fourth student is not moved.
 In total, 1 + 3 + 0 + 0 = 4 moves were used.
- 
-
 Constraints:
-
 n == seats.length == students.length
 1 <= n <= 100
 1 <= seats[i], students[j] <= 100 */
@@ -60,6 +49,8 @@ public class minimumNumberofMovestoSeatEveryone {
             seats[i] = seats[minIndex];
             seats[minIndex] = temp;
         }
+
+     
      int result = 0;
         for (int i = 0; i < seats.length; i++) {
             result += Math.abs(seats[i] - students[i]);         
