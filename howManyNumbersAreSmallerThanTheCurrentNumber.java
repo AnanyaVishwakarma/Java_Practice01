f35707bba84f3ae85a88f6c982e0f838d60e3c12
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-08-04 22:59:13 +0530

    Update howManyNumbersAreSmallerThanTheCurrentNumber.java

--- howManyNumbersAreSmallerThanTheCurrentNumber.java
+++ howManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -41,13 +41,23 @@ public class howManyNumbersAreSmallerThanTheCurrentNumber {
     return answer;
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         howManyNumbersAreSmallerThanTheCurrentNumber obj = new howManyNumbersAreSmallerThanTheCurrentNumber();
-        int[] nums = {8, 1, 2, 2, 3};
+        System.out.print("Enter the number of elements you want in you array: " );
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        System.out.print("Enter " + n + " numbers: ");
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
         System.out.println(Arrays.toString(obj.smallerNumbersThanCurrent(nums)));
         int[] nums2 = {6,5,4,8};
         System.out.println(Arrays.toString(obj.smallerNumbersThanCurrent(nums2)));
     }
 }
+
 
 
 
