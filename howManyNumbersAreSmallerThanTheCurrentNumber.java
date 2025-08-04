f850a7da70350d689ac95d58fcc3a2f0b40d2d76
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-08-04 22:49:12 +0530

    Update howManyNumbersAreSmallerThanTheCurrentNumber.java

--- howManyNumbersAreSmallerThanTheCurrentNumber.java
+++ howManyNumbersAreSmallerThanTheCurrentNumber.java
@@ -41,6 +41,7 @@ public class howManyNumbersAreSmallerThanTheCurrentNumber {
             }
         }
         for (int i = 0; i < n; i++) {
+            int count = 0;
             for (int j = i + 1; j < n; j++) {
                 if (nums[j] < nums[i]) {
                     int sum = 0;
@@ -52,3 +53,4 @@ public class howManyNumbersAreSmallerThanTheCurrentNumber {
         }   
     return answer;
 }
+
