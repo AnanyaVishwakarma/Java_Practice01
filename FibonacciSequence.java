17df3db632151bdcb90093002656878254268496
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-08-05 22:41:45 +0530

    Update FibonacciSequence.java

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -1,2 +1,8 @@
 /*The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones, 
 typically starting with 0 and 1. The sequence begins: 0, 1, 1, 2, 3, 5, 8, 13, 21, and so on.*/
+import java.util.*;
+
+public class FibonnacciSequence {
+    public int[] fibonacciSeries (int n){
+        int[] nums = new int[n];
+}
