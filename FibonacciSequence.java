06eaa458bb1d5b0f654b8856e2d575fa23d1ceb4
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-08-05 22:42:47 +0530

    Update FibonacciSequence.java

--- FibonacciSequence.java
+++ FibonacciSequence.java
@@ -9,5 +9,9 @@ public class FibonnacciSequence {
         if(n >= 2) nums[1] = 1;
 
         for(int i = 2; i < n; i++){
+            nums[i] = nums[i-1] + nums[i -2];
+        }
+        return nums;
+        }
     }    
 }
