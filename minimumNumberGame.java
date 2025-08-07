c6f723c84da3ab1201935cf0ae606d5a33155247
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-08-07 22:28:12 +0530

    Update minimumNumberGame.java

--- minimumNumberGame.java
+++ minimumNumberGame.java
@@ -24,8 +24,12 @@ public class minimumNumberGame {
     for(int i = 0; i <n; i++){
         int minIndex = i;
         for(int j = i+1; j<n; j++){
+           if(nums[j] < nums[minIndex]){
+                minIndex = j;
+            }
  }     
 }
+
 
 
 
