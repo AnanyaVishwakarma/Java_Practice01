664cf5e96a7998e8ada3d16943aee43c70b581c4
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-08-25 23:11:00 +0530

    Update MinimumAverageOfSmallestAndLargestElements.java

--- MinimumAverageOfSmallestAndLargestElements.java
+++ MinimumAverageOfSmallestAndLargestElements.java
@@ -42,6 +42,7 @@ public class MinimumAverageOfSmallestAndLargestElements {
     public double minimumAverage(int[] nums) {
         int n = nums.length;
         int[] array = new int [n/2];
+        
         for (int i = 0; i < n; i++){
             int minIndex = i;
             for(int j = i; j < n; j++){
@@ -68,6 +69,7 @@ public class MinimumAverageOfSmallestAndLargestElements {
     }
            
 }
+
 
 
 
