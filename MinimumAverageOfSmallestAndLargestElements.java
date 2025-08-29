cbfd86c0c75df5115a7ebc7621950e93067046e5
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-08-29 22:05:28 +0530

    Update MinimumAverageOfSmallestAndLargestElements.java

--- MinimumAverageOfSmallestAndLargestElements.java
+++ MinimumAverageOfSmallestAndLargestElements.java
@@ -82,9 +82,13 @@ public class MinimumAverageOfSmallestAndLargestElements {
         int n = sc.nextInt();
         int[] nums = new int[n];
         System.out.println("Enter the elements: ");
-}
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
+    }
            
 }
+
 
 
 
