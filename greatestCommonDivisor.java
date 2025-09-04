50083a1d4cdc47366684fd3636cc20640e8c816e
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-09-04 21:50:55 +0530

    Update greatestCommonDivisor.java

--- greatestCommonDivisor.java
+++ greatestCommonDivisor.java
@@ -7,8 +7,13 @@ public class greatestCommonDivisor {
     }
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+   
     }
 }
+
 
 
 
