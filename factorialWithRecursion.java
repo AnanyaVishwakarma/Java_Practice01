270a7d1f21ba4d84c3fd2514a33b27742c896fb8
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-09-04 22:03:48 +0530

    Update factorialWithRecursion.java

--- factorialWithRecursion.java
+++ factorialWithRecursion.java
@@ -4,9 +4,12 @@ public class factorialWithRecursion {
     public int checkFactorial(int n){
         if(n <= 1){
             return n;
-}
+        }
+        return checkFactorial(n*(n-1));
+     }
     
 }
+
 
 
 
