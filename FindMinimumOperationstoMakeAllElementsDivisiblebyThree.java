ac731dcecc412381f3140ea5acdc52205d66653f
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-09-06 23:39:21 +0530

    Update FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java

--- FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
+++ FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
@@ -21,9 +21,11 @@ public class FindMinimumOperationstoMakeAllElementsDivisiblebyThree {
     }   
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-            
+    vendinMachineChange obj = new vendinMachineChange();
+        
     }     
 }
+
 
 
 
