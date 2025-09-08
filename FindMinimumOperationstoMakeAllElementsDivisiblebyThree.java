b81720438c575ce8a8263548ba45bb970fc5b535
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-09-08 23:29:37 +0530

    Update FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java

--- FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
+++ FindMinimumOperationstoMakeAllElementsDivisiblebyThree.java
@@ -34,11 +34,12 @@ public class FindMinimumOperationstoMakeAllElementsDivisiblebyThree {
     int[] nums = new int[n];         
         for(int i = 0; i < n; i++){
           nums[i] = sc.nextInt();
-        }         System.out.print(obj.minimumOperations(nums));
-                 
+        }        
+         System.out.print(obj.minimumOperations(nums));        
     }     
      
 }
+
 
 
 
