72a8903b71094f9edfbcfb276c1568604406ce79
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-09-15 23:37:56 +0530

    Update SplitStringBySeparator.java

--- SplitStringBySeparator.java
+++ SplitStringBySeparator.java
@@ -54,10 +54,13 @@ public class SplitStringBySeparator {
   SplitStringBySeparator obj = new SplitStringBySeparator();
   Scanner sc = new Scanner(System.in);
   int n = sc.nextInt();
-  String[] = new String[n];
-  for(int i = 0; i < n; i++){}
+  String[] words = new String[n];
+  for(int i = 0; i < n; i++){
+   
+  }
  }
 }
+
 
 
 
