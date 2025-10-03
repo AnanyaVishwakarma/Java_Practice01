c2ab98690b627e5a87b3878b382e510f9d018b4b
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-10-03 23:44:58 +0530

    Update WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java

--- WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
+++ WidestVerticalAreaBetweenTwoPointsContainingNoPoints.java
@@ -19,12 +19,14 @@ points[i].length == 2
 public class WidestVerticalAreaBetweenTwoPointsContainingNoPoints {
     public int maxWidthOfVerticalArea(int[][] points) {\
         int n = points.length;
+        int[] xCoords =
         
     }   
     public static void main(String[] args) {
         
     }     
 }
+
 
 
 
