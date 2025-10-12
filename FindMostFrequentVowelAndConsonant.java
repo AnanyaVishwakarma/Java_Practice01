48920f8af42f5c04f930bfebdc4eb63cd6cf9bb3
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-10-12 13:37:58 +0530

    Update FindMostFrequentVowelAndConsonant.java

--- FindMostFrequentVowelAndConsonant.java
+++ FindMostFrequentVowelAndConsonant.java
@@ -28,11 +28,12 @@ import java.util.*;
 public class FindMostFrequentVowelAndConsonant {
     public int maxFreqSum(String s) {
         Map<Character, Integer> chMap = new Map<Character,Integer>(); 
-        
+        for(int i = 0; i < s.length(); i++){}        
         
     }
     public static void main(String[] args){
         
     }
 }
+
 
