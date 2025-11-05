98cc3170c0779707571f276683b5c8aca7063940
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-11-05 23:07:50 +0530

    Update theTwoSneakyNumbersOfDigitville.java

--- theTwoSneakyNumbersOfDigitville.java
+++ theTwoSneakyNumbersOfDigitville.java
@@ -40,8 +40,12 @@ public class theTwoSneakyNumbersOfDigitville {
 
         int n = sc.nextInt();
         int[] nums = new int[n];
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
     }
 }
+
 
 
 
