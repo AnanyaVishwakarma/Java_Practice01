6b9ce5b1067f875fb923b6df4b4a4fcc997289df
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-11-07 23:57:17 +0530

    Update CounttheNumberofConsistentStrings.java

--- CounttheNumberofConsistentStrings.java
+++ CounttheNumberofConsistentStrings.java
@@ -37,9 +37,11 @@ public class CounttheNumberofConsistentStrings {
     }   
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        CounttheNumberofConsistentStrings obj = new CounttheNumberofConsistentStrings();
       
     }    
 }
+
 
 
 
