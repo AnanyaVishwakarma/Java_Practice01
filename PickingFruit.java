882ee5d202b85829f60ef680c19db9716ec2eefb
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-12-01 23:55:28 +0530

    Add files via upload

---
+++ PickingFruit.java
@@ -0,0 +1,4 @@
+/*You are given an integer array orchard, where orchard[i] is the fruit type on the i-th tree.
+You have two baskets, each holding unlimited fruit of a single type.
+You may start at any index and must pick one fruit from each consecutive tree to the right, stopping when a tree produces a fruit that doesn’t fit the two types you’re carrying.
+Return the maximum number of fruits you can pick.(ananya)*/
