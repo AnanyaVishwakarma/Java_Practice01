46e0316600f8c05e27a910f351d7eec066bd3314
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-12-01 23:56:50 +0530

    Update PickingFruit.java

--- PickingFruit.java
+++ PickingFruit.java
@@ -3,5 +3,8 @@ You have two baskets, each holding unlimited fruit of a single type.
 You may start at any index and must pick one fruit from each consecutive tree to the right, stopping when a tree produces a fruit that doesn’t fit the two types you’re carrying.
 Return the maximum number of fruits you can pick.(ananya)*/
 public class PickingFruit {
-   
+public static void main(String[] args) {
+    
+}      
 }
+
