2ff59f79b46cc76e3f0f9d605612223207a4258c
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-12-20 23:14:13 +0530

    Update FindThePrefixCommonArray.java

--- FindThePrefixCommonArray.java
+++ FindThePrefixCommonArray.java
@@ -30,8 +30,11 @@ public class FindThePrefixCommonArray {
         }
      
     }    
-    public static void main(String[] args)
+    public static void main(String[] args){
+        
+    }
 }
+
 
 
 
