bfbe9d38bc87c9d021d8dc8b6e459471535a4f02
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-12-25 23:53:05 +0530

    Update findAllNumbersInTheArray.java

--- findAllNumbersInTheArray.java
+++ findAllNumbersInTheArray.java
@@ -30,9 +30,12 @@ public class findAllNumbersInTheArray {
         findAllNumbersInTheArray obj =  new findAllNumbersInTheArray();
         int n = sc.nextInt();
         int[] nums = new int[n];
-        for(
+        for(int i = 0; i < n; i++){
+            nums[i] = sc.nextInt();
+        }
         
     }
 }
+
 
 
