d72f791b92e11f94ff07bf2c662a96d37a518841
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-12-25 23:50:37 +0530

    Update findAllNumbersInTheArray.java

--- findAllNumbersInTheArray.java
+++ findAllNumbersInTheArray.java
@@ -1,17 +1,11 @@
 /*Given an array nums of n integers where nums[i] is in the range [1, n], 
 return an array of all the integers in the range [1, n] that do not appear in nums.
-
- 
-
 Example 1:
-
 Input: nums = [4,3,2,7,8,2,3,1]
 Output: [5,6]
 Example 2:
-
 Input: nums = [1,1]
 Output: [2 */
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,3 +27,4 @@ public class findAllNumbersInTheArray {
     }
 
 }
+
