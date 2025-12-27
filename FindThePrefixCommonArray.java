50056f3aae347fdc37b9d520ef47fab22df69e52
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-12-27 22:52:50 +0530

    Update FindThePrefixCommonArray.java

--- FindThePrefixCommonArray.java
+++ FindThePrefixCommonArray.java
@@ -47,11 +47,15 @@ public class FindThePrefixCommonArray {
         for(int i=0; i < n; i++){
             A[i] = sc.nextInt();
         }
-        for(int i = 0; i< n; i++){}
+        for(int i = 0; i< n; i++){
+            B[i] = sc.nextInt();
+        }
+        System.out.println();
     
         
     }
 }
+
 
 
 
