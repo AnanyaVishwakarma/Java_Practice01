bd4ff2b6ec0ac53a778018468ad59b698fea9cd8
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2025-12-30 22:46:30 +0530

    Update Currency_formatter.java

--- Currency_formatter.java
+++ Currency_formatter.java
@@ -1,34 +1,24 @@
 /*Given a double-precision number,'payment' , denoting an amount of money, use the NumberFormat class' 
 getCurrencyInstance method to convert 'payment' into the US, Indian, Chinese, and French currency formats. 
 Then print the formatted values as follows:
-
 US: formattedPayment
 India: formattedPayment
 China: formattedPayment
 France: formattedPayment
 where 'formatted payment' is 'payment' formatted according to the appropriate Locale's currency.
-
 Note: India does not have a built-in Locale, so you must construct one where the language is en (i.e., English).
 Input Format:
-
 A single double-precision number denoting 'payment'.
-
 Constraints:
 0 <= payment <= 10^9
-
 Output Format:
-
 On the first line, print US: u where  is  formatted for US currency.
 On the second line, print India: i where  is  formatted for Indian currency.
 On the third line, print China: c where  is  formatted for Chinese currency.
 On the fourth line, print France: f, where  is  formatted for French currency.*/
-
 import java.util.*;
 import java.text.*;
-
 public class Currency_formatter {
-    
-    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double payment = scanner.nextDouble();
@@ -55,3 +45,4 @@ public class Currency_formatter {
         System.out.println("France: " + france.format(payment));
     }
 }
+
