9c5a3217f4f72d7ee26bd5de2861065a517cda28
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-09 23:29:30 +0530

    commited a modified file

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -1,41 +1,28 @@
-/*Given an integer array nums and an integer k, return true 
-if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+/*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
 Example 1:
-Input: nums = [1,2,3,1], k = 3
+
+Input: nums = [1,2,3,1]
 Output: true
 Example 2:
-Input: nums = [1,0,1,1], k = 1
-Output: true
-Example 3:
-Input: nums = [1,2,3,1,2,3], k = 2
+
+Input: nums = [1,2,3,4]
 Output: false
-Constraints:
-1 <= nums.length <= 105
--109 <= nums[i] <= 109
-0 <= k <= 105 */
-public class containsDuplicate {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer, Integer> indexMap = new HashMap <>();
-        for(int i = 0; i <n; i++){
-            int num = nums[i];
-                if(indexMap.containsKey(num)){
-                int lastIndex = indexMap.get(num);
-                if(i - lastIndex <= k){
-                    return true;
-                }
-           }
-            indexMap.put(num, i);
-    }
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true */
+
+public class ContainsDuplicate {
+    public boolean ContainsDuplicate(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for (Integer num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
         return false;
-    }   
-     public static void main(String[] args) {
-         containsDuplicate cd = new containsDuplicate();
-         int nums[] = {1,2,3,1};
-         int k = 3;
-         System.out.println(cd.containsNearbyDuplicate(nums, k));
-         int nums1[] = {1,2,3,1,2,3};
-         int k1 = 2;
-         System.out.println(cd.containsNearbyDuplicate(nums1, k1));
-     }
+    }
 }
