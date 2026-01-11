fddbfc06a622aacc7f8fe363b10af6f2e1a43934
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-11 22:25:25 +0530

    commit

--- maximumSubstringsWithDistinctStart.java
+++ maximumSubstringsWithDistinctStart.java
@@ -30,6 +30,8 @@ public class maximumSubstringsWithDistinctStart {
         int n = s.length();
         int count = 0;
         for (int i = 0; i <=n; i++){
+            String substr = s.substring(0);
+                        
         }
         return count;
     }    
