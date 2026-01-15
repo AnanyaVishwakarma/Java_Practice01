2360a5724c03c561270db7c5be08f15c7922050b
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2026-01-16 00:00:06 +0530

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -1,9 +1,5 @@
 /*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
- 
-
 Example 1:
-
 Input: nums = [1,2,3,1]
 Output: true
 Example 2:
@@ -25,4 +21,5 @@ public class ContainsDuplicate {
         return false;
     }
 }
+
 
