62b894cbf4849ac1358e416bdb97994d4a982f1e
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-16 23:10:09 +0530

    Merge

--- containsDuplicate.java
+++ containsDuplicate.java
@@ -1,9 +1,5 @@
 /*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
-
- 
-
 Example 1:
-
 Input: nums = [1,2,3,1]
 Output: true
 Example 2:
@@ -14,7 +10,6 @@ Example 3:
 
 Input: nums = [1,1,1,3,3,4,3,2,4,2]
 Output: true */
-
 public class ContainsDuplicate {
     public boolean ContainsDuplicate(int[] nums){
         HashSet<Integer> set = new HashSet<>();
@@ -26,3 +21,5 @@ public class ContainsDuplicate {
         return false;
     }
 }
+
+
