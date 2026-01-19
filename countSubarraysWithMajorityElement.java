93fcce38b7bc68799318fad26709543ae0b296f5
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-20 00:56:50 +0530

    Another loop

--- countSubarraysWithMajorityElement.java
+++ countSubarraysWithMajorityElement.java
@@ -32,8 +32,10 @@ public class countSubarraysWithMajorityElement {
         int count = 0;
         int[] arr = new int[n];
         for(int i=0; i < n; i++){
-            for(int j = n; j > i; j++){
-                
+            for(int j = n; j > 0; j--){
+                for(int k = i; k < j; k++){
+
+                }
             }
         }
      
