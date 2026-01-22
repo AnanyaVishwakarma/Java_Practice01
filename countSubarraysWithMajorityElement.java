cdbefbc4a1cdaa57d1f6d5efdbb67d8598e18bce
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-22 20:54:48 +0530

    condition for the sum of subarrays

--- countSubarraysWithMajorityElement.java
+++ countSubarraysWithMajorityElement.java
@@ -39,7 +39,10 @@ public class countSubarraysWithMajorityElement {
         }
         int count = 0;
         for(int i = 0; i < n; i++){
-            
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                
+            }
         }
         
     }    
