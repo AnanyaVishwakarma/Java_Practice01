f5dcf69f7628a8a8223761f5ee16d78c09803af8
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-25 23:18:17 +0530

    fetching user input in array

--- computeAlternatingSum.java
+++ computeAlternatingSum.java
@@ -46,8 +46,9 @@ public class computeAlternatingSum {
         int n = sc.nextInt();
         int[] nums = new int[n];
         for(int i =0; i < n; i++){
-            
+            nums[i] = sc.nextInt();
         }
+        
     }
     
     
