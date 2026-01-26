e5e7c55f3ff452c50a43724eb90d0870cebb45b6
AnanyaVishwakarma <vananya557@gmail.com>
2026-01-26 23:55:43 +0530

    computation in logic

--- computeAlternatingSum.java
+++ computeAlternatingSum.java
@@ -30,9 +30,9 @@ public class computeAlternatingSum {
         int sum = 0;
         for(int i = 1; i < n; i++){
             if(i % 2 == 0){
-                sum
+                sum += nums[i];
             } else {
-                 
+                sum -= nums[i];
             }
         }
         
