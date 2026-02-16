7e79d9738eb3cd15f4944e05e965eaf67d9e4426
AnanyaVishwakarma <vananya557@gmail.com>
2026-02-16 23:25:32 +0530

    importing Arrays package

--- MaximumProductOfTwoElementsInAnArray.java
+++ MaximumProductOfTwoElementsInAnArray.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 /*Given the array of integers nums, you will choose two different indices i and j of that array. 
 Return the maximum value of (nums[i]-1)*(nums[j]-1).
 Example 1:
