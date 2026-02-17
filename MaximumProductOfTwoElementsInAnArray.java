a7e45c60645dd32b534116890552fe75976c1720
AnanyaVishwakarma <vananya557@gmail.com>
2026-02-17 22:43:52 +0530

    too complicated

--- MaximumProductOfTwoElementsInAnArray.java
+++ MaximumProductOfTwoElementsInAnArray.java
@@ -22,14 +22,7 @@ Constraints:
 public class MaximumProductOfTwoElementsInAnArray {
     public int maxProduct(int[] nums) {
         Arrays.sort(nums);
-        int product = 0;
-        for(int i = nums.length-1; i > 0;i--){
-            for(int j = i - 1; j > 0;){
-                product = (nums[i]-1) * (nums[j]-1);
-                break;
-            }
-        }
-        return product;
+        
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
