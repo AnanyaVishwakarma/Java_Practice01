2b739c1cfa7cb489f1e9d3c66b0a7b1dcff92d24
AnanyaVishwakarma <vananya557@gmail.com>
2026-02-19 22:26:11 +0530

    Problem commented, Class initated

---
+++ twoWheelersOrFourWheelers.java
@@ -0,0 +1,6 @@
+/*An automobile company produces two types of vehicles: Two-Wheelers (TW) and Four-Wheelers (FW). You are given the total number of 
+vehicles (V) and the total number of wheels (W). Write a program to find the number of TW and FW. 
+Input: V = 200, W = 540 Output: TW = 130, FW = 70Edge Case: If W < 2, W is odd, or W \leq V, print "INVALID INPUT".*/
+public class twoWheelersOrFourWheelers {
+    
+}
