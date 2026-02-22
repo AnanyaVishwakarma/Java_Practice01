ceeff13a7fc131951170cd8b16f35079a83bea71
AnanyaVishwakarma <vananya557@gmail.com>
2026-02-22 22:58:55 +0530

    for loop

--- sumOfIntegers.java
+++ sumOfIntegers.java
@@ -2,6 +2,10 @@ public class sumOfIntegers {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
-        int[] 
+        int[] integers = new int[k];
+
+        for(int i = 0; i < k; i++){
+
+        }
     }
 }
