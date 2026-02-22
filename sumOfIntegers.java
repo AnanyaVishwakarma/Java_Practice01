7c5bc87a7791277985a8d2dcbf1d22009b811581
AnanyaVishwakarma <vananya557@gmail.com>
2026-02-22 22:56:00 +0530

    Scanning the input

--- sumOfIntegers.java
+++ sumOfIntegers.java
@@ -1,5 +1,7 @@
 public class sumOfIntegers {
     public static void main(String[] args){
-        
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        int[] 
     }
 }
