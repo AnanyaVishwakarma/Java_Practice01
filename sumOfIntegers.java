e5345a6418299b2b2303da9c3f86c080b98b4f18
AnanyaVishwakarma <vananya557@gmail.com>
2026-02-23 23:05:07 +0530

    edge case

--- sumOfIntegers.java
+++ sumOfIntegers.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class sumOfIntegers {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
         int[] integers = new int[k];
+        int sum = 0;
 
         for(int i = 0; i < k; i++){
-
+            integers[i] = sc.nextInt();
+        }
+        if(k < 2){
+            System.out.println("Invalid input");
         }
     }
 }
