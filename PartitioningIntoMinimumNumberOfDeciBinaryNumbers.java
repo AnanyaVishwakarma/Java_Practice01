7143332e468e3082146e751adc348aa7ce16d7fd
AnanyaVishwakarma <vananya557@gmail.com>
2026-03-04 23:38:31 +0530

    Variable

--- PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
+++ PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -16,9 +16,10 @@ n consists of only digits.
 n does not contain any leading zeros and represents a positive integer. */
 /*GREEDY METHOD */
 public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
-    public static void main(String[] args){
-        String n = 32;
-        
+    public static void main(String[] args) {
+        String n = "32";
+        int max = 0;
+
     }
-    
+
 }
