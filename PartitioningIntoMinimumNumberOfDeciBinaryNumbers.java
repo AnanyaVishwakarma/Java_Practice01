20bbbc28fa7b69df1ff648aa51620661d1474da8
AnanyaVishwakarma <vananya557@gmail.com>
2026-03-07 00:30:30 +0530

    .

--- PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
+++ PartitioningIntoMinimumNumberOfDeciBinaryNumbers.java
@@ -21,8 +21,10 @@ public class PartitioningIntoMinimumNumberOfDeciBinaryNumbers {
         int max = 0;
         for (int i = 0; i < n.length(); i++) {
             int digit = n.charAt(i) - '0';
+            if (digit > max) {
+                max = digit;
+            }
         }
-
     }
 
 }
