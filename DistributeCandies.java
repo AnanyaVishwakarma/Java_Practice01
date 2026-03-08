9c58ff9ebdb720dd5b72d7205caa6fd4bc664097
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2026-03-09 00:06:29 +0530

    Update DistributeCandies.java

--- DistributeCandies.java
+++ DistributeCandies.java
@@ -4,26 +4,18 @@ to gain weight, so she visited a doctor.
 The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). 
 Alice likes her candies very much, and she wants to eat the maximum number of different types of 
 candies while still following the doctor's advice.
-
 Given the integer array candyType of length n, return the maximum number of different types of candies
 she can eat if she only eats n / 2 of them.
-
- 
-
 Example 1:
-
 Input: candyType = [1,1,2,2,3,3]
 Output: 3
 Explanation: Alice can only eat 6 / 2 = 3 candies. Since there are only 3 types, she can eat one of each type.
 Example 2:
-
 Input: candyType = [1,1,2,3]
 Output: 2
 Explanation: Alice can only eat 4 / 2 = 2 candies. Whether she eats types
  [1,2], [1,3], or [2,3], she still can only eat 2 different types. */
-
 import java.util.HashSet;
-
 public class DistributeCandies {
     public int distributeCandies(int[] candyType) {
         int n = candyType.length;
@@ -34,3 +26,4 @@ public class DistributeCandies {
          return Math.min(uniqueCandiesSet.size(), n/2);  
     } 
 }
+
