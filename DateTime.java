ebeb1a37e5e3d8b47df7809ec7d10ef0a3bf1a29
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2026-04-03 23:15:35 +0530

    Update DateTime.java

--- DateTime.java
+++ DateTime.java
@@ -1,6 +1,6 @@
 /*You are given a date. You just need to write the method, "getDay", which returns the day on that date. 
 To simplify your task, we have provided a portion of the code in the editor.
-Example
+Example:
 month=8
 day=14
 year=2017
