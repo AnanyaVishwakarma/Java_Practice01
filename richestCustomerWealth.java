9da689cae310349742bc2e553c51f9b396c3610d
AnanyaVishwakarma <vananya557@gmail.com>
2026-04-09 23:59:09 +0530

    mistake

--- richestCustomerWealth.java
+++ richestCustomerWealth.java
@@ -2,7 +2,7 @@ public class richestCustomerWealth {
         public int maximumWealth(int[][] accounts) {
         
     }
-    public public static void main(String[] args) {
+    public static void main(String[] args) {
         
     }
 
