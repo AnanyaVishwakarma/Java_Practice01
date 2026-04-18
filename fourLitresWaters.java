cf723d10826445c2eeb708d7e97c5e2eef270e91
AnanyaVishwakarma <vananya557@gmail.com>
2026-04-18 23:56:00 +0530

    Algorithm to solve the question

---
+++ fourLitresWaters.java
@@ -0,0 +1,8 @@
+/*Algorithm
+fill the '5l' bucket to the top
+transfer the '5l' to '3l' bucket
+//you have 2litres left in '5l' bucket//
+transfer the 2litres left in '5l' bucket
+repeat first and second step
+transfer the initial saved 2 litres into current 2litres
+*/
