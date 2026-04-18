f5c24ec9449647ad68242d9eb21d5438fa74af70
AnanyaVishwakarma <vananya557@gmail.com>
2026-04-19 00:42:56 +0530

    brute force method

--- fourLitresWaters.java
+++ fourLitresWaters.java
@@ -6,3 +6,18 @@ transfer the 2litres left in '5l' bucket
 repeat first and second step
 transfer the initial saved 2 litres into current 2litres
 */
+public class fourLitresWaters {
+    public static void main(String[] args) {
+        int bucket1 = 5; 
+        int bucket2 = 3;
+        int temp;
+
+        temp = bucket1 - bucket2;
+        bucket1 = bucket1 - bucket2;
+        bucket1 = bucket1 + temp;
+        System.out.println(bucket1);
+
+    }
+
+    
+}
