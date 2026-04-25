6762b1e23ecb2e1cf2e3ecf7f611218d2a250ef4
Ananya Vishwakarma <166329794+AnanyaVishwakarma@users.noreply.github.com>
2026-04-25 23:42:13 +0530

    Correct loop bounds for accounts input
    Fix loop condition to prevent ArrayIndexOutOfBoundsException.

--- richestCustomerWealth.java
+++ richestCustomerWealth.java
@@ -28,7 +28,11 @@ public class richestCustomerWealth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] accounts = new int[3][3];
-
+        for(int i =0; i <= 3; i++){
+            for(int j = 0; j < 3; j++){
+                accounts[i][j] = sc.nextInt();
+            }
+        }
         
         int max = 0;
         for(int i =0; i < 3; i++){
