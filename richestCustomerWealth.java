edf6b29d4d9d253b0c3f81c96c144bb8cc60c5ab
AnanyaVishwakarma <vananya557@gmail.com>
2026-05-04 23:36:36 +0530

    .

--- richestCustomerWealth.java
+++ richestCustomerWealth.java
@@ -55,5 +55,6 @@ public class richestCustomerWealth {
         }
        }
        richestCustomerWealth solution = new richestCustomerWealth();
+         int result = solution.maximumWealth(accounts);
     }   
 }
