3c4b96fc83e466a6e1461c8fe6f676a2b1894c5b
AnanyaVishwakarma <vananya557@gmail.com>
2026-05-07 00:37:39 +0530

    error fixing

--- threeSum.java
+++ threeSum.java
@@ -1,12 +1,7 @@
 /*Given an integer array nums, return all the triplets [nums[i], nums[j], 
 nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-
 Notice that the solution set must not contain duplicate triplets.
-
- 
-
 Example 1:
-
 Input: nums = [-1,0,1,2,-1,-4]
 Output: [[-1,-1,2],[-1,0,1]]
 Explanation: 
@@ -16,21 +11,21 @@ nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 The distinct triplets are [-1,0,1] and [-1,-1,2].
 Notice that the order of the output and the order of the triplets does not matter.
 Example 2:
-
 Input: nums = [0,1,1]
 Output: []
 Explanation: The only possible triplet does not sum up to 0.
 Example 3:
-
 Input: nums = [0,0,0]
 Output: [[0,0,0]]
 Explanation: The only possible triplet sums up to 0. */
 
+import java.util.Arrays;
 import java.util.List;
 
 public class threeSum {
     public List<List<Integer>> threeSum(int[] nums) {
        Arrays.sort(nums);
+        List<List<Integer>> result = null;
 
         for(int i = 0;i < nums.length-2;i++){
             if(i > 0 && nums[i] == nums[i - 1]){
