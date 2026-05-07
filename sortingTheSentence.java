9979506d0206d1136cbfa3c223b1d820e6ccdf47
AnanyaVishwakarma <vananya557@gmail.com>
2026-05-07 23:41:16 +0530

    error fixing

--- sortingTheSentence.java
+++ sortingTheSentence.java
@@ -22,7 +22,7 @@ public class sortingTheSentence {
         String[] str = s.split(" ");
         String[] result = new String[str.length];
 
-        for(String word: words){
+        for(String word: str){
             int pos = word.charAt(word.length() - 1) - '0';
 
             result[pos - 1] = word.substring(0, word.length() - 1);
