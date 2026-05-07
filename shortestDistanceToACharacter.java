3976d791a751a490e4221169f7d21cf70b213533
AnanyaVishwakarma <vananya557@gmail.com>
2026-05-07 23:59:43 +0530

    main method initalised

--- shortestDistanceToACharacter.java
+++ shortestDistanceToACharacter.java
@@ -1,12 +1,7 @@
 /*Given a string s and a character c that occurs in s, return an array of integers answer where answer.length == s.length 
 and answer[i] is the distance from index i to the closest occurrence of character c in s.
-
 The distance between two indices i and j is abs(i - j), where abs is the absolute value function.
-
- 
-
 Example 1:
-
 Input: s = "loveleetcode", c = "e"
 Output: [3,2,1,0,1,0,0,1,2,2,1,0]
 Explanation: The character 'e' appears at indices 3, 5, 6, and 11 (0-indexed).
@@ -16,11 +11,8 @@ For index 4, there is a tie between the 'e' at index 3 and the 'e' at index 5, b
 same: abs(4 - 3) == abs(4 - 5) = 1.
 The closest occurrence of 'e' for index 8 is at index 6, so the distance is abs(8 - 6) = 2.
 Example 2:
-
 Input: s = "aaab", c = "b"
 Output: [3,2,1,0] */
-
-
 public class shortestDistanceToACharacter {
     public int[] shortestToChar(String s, char c) {
         int n = s.length();
@@ -43,6 +35,14 @@ public class shortestDistanceToACharacter {
         }
         return answer;
     }  
+    public static void main(String[] args) {
+        shortestDistanceToACharacter obj = new shortestDistanceToACharacter();
+        String s = "loveleetcode";
+        char c = 'e';
+        int[] result = obj.shortestToChar(s, c);
+        for (int i : result){
+            System.out.print(i + " ");
+        }
+    }
 
     }    
-}
